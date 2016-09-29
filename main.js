9dacc0e744ef0dc3b9b68b0880de9ae33261a2a6
Peter Elbaum <elbaumpj@gmail.com>
2016-09-29 16:14:21 -0400

    Finished fourth question

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+(function() {
+"use strict"
+
 // 1.
 
 var allPrices = items.map(function(item){
@@ -16,12 +19,12 @@ console.log("The average price is: " + averagePrice);
 //2.
 
 var priceRange = items.filter(function(item){
-  var range = item["price"] >= 14 && item["price"] <= 18;
+  var range = item["price"] > 14 && item["price"] < 18;
   return range;
 });
 //the function targets the price of each item and then on line 17 filters those items to return those that have a
 //price range between 14 and 18
-console.log("Items that cost between $14.00 and $18.00 USD: " + priceRange);
+console.log("Items that cost between $14.00 and $18.00 USD: " , priceRange);
 
 //3.
 var britishPounds = items.filter(function(item){
@@ -29,10 +32,32 @@ var britishPounds = items.filter(function(item){
 });
 //this function finds the one item with a currency code of GBP
 var priceSentence = britishPounds.forEach(function(item){
- console.log(item["title"] + " costs " + " £" + item["price"]);
+ console.log(item["title"] + " costs " + "£" + item["price"]);
 });
 //here I use forEach loop to print the title and price of the object
-//I realize this is a bit ridiculous for one object, but this code would scale if there were more than one object
+//I realize it is a bit ridiculous for one object, but this code would scale if there were more than one object
 // that used GPB.
 
 //4.
+
+var woodItems = items.filter(function(item){
+  if(item["materials"].indexOf("wood") != -1) {
+    return item;
+  }
+});
+// filters the list of items and uses indexOf to return the objects that do have wood
+var printItems = woodItems.forEach(function(item){
+  console.log(item["title"]);
+ });
+//prints the titles of each item that was stored in the previous function 
+
+//5.
+
+
+
+
+
+
+
+
+}());
